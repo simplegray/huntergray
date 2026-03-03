d6d13cb9c5f6e8713d7138f58e5c46ba58ec9d97
Hunter Gray <simplegray@gmail.com>
2026-03-03 12:54:04 -0800

    fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/huntergray/',
   server: {
     host: "::",
     port: 8080,
